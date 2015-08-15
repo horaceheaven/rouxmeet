84fd525a20e746d3944e5e7c7f8c3d246b28a248
horaceheaven <hheaven@medullan.com>
2015-08-15 12:27:05 -0400

    adding ejs to template for dynamic data

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.locals.appdata = require('./data.json');
+
 app.use('/', routes);
 
 // catch 404 and forward to error handler
